d1ea973c1cef6dbc6bb50999fe3fdb9d7be7ca07
Eve Stockstill <evestockstillpdx@gmail.com>
2019-09-29 11:07:49 -0700

    final

---
+++ app.js
@@ -0,0 +1,19 @@
+function flip1 () {
+    document.getElementById('t1').innerHTML = `Created on a separate text file and saved with a .css extension`;
+}
+function flip2 () {
+    document.getElementById('t2').innerHTML = `Designates what element you want to change such as width, height, color, and font`;
+
+}
+function flip3() {
+    document.getElementById('t3').innerHTML = `Defines what settings you want for the properties. Example specifing font you want for the font family `;
+}
+function flip4() {
+    document.getElementById('t4').innerHTML = `Selector and Declaration.`;
+}
+function flip5() {
+    document.getElementById('t5').innerHTML = `If one selector is more specific than another one it will overide others, like a hierarchy. Type selectors such as h1 is less than a .(class) selector and an #(ID) selector is higher than a .class selector`;
+}
+function flip6() {
+    document.getElementById('t6').innerHTML = `If selectors have equivalent specificity, then the selector that appears last will win`;
+}
